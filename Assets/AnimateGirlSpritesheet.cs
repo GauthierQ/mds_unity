205172753bd7727d01669ca4a8700391c28527c9
GauthierQ <queau.gauthier@orange.fr>
2020-09-30 16:17:04 +0200

    first

---
+++ Assets/AnimateGirlSpritesheet.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpritesheetAnimator))]
+public class AnimateGirlSpritesheet : MonoBehaviour
+{
+    SpritesheetAnimator animator;
+
+    void Start()
+    {
+        animator = GetComponent<SpritesheetAnimator>();
+    }
+
+
+    void Update()
+    {
+
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 0, 0);
+            animator.Play(Anims.Run);
+        }
+        else if (Input.GetKey(KeyCode.LeftShift))
+        {
+            animator.Play(Anims.Roll);
+        }
+        else if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localRotation = Quaternion.Euler(0, 180, 0);
+            animator.Play(Anims.Run);
+        }
+        else if (Input.GetKey(KeyCode.DownArrow))
+        {
+            animator.Play(Anims.Run);
+        }
+        else if (Input.GetKey(KeyCode.UpArrow))
+        {
+            animator.Play(Anims.Run);
+        }
+        else if (Input.GetKey(KeyCode.Space))
+        {
+            animator.Play(Anims.Jump);
+        }
+        else
+        {
+            animator.Play(Anims.Iddle);
+        }
+    }
+}
